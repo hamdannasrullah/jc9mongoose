2228e32180f9e12ec6db6a91bd86008f72b5b266
hamdannasrullah <hamdan.nasrullah@ymail.com>
2019-08-01 11:52:24 +0700

    push heroku

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const cors = require ('cors')
 const User = require('./models/user')
 const Task = require('./models/task')
 
-mongoose.connect('mongodb://127.0.0.1:27017/jc-mongoose', {
-    // mongoose.connect('mongodb+srv://hamdan:pekalongan@jc9cluster-fcj7z.mongodb.net/JC9Database?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb://127.0.0.1:27017/jc-mongoose', {
+    mongoose.connect('mongodb+srv://hamdan:pekalongan@jc9cluster-fcj7z.mongodb.net/JC9Database?retryWrites=true&w=majority', {
     // Parser string URL
     useNewUrlParser: true,
 
